ab3c012c249a139807dbc98a2d36ce6d22fb3250
Evgeny Vovchenko <evgeny.vovchenko@act.org>
2019-07-29 20:44:34 +0300

    move augerAPI to separate file

--- pyspark_optimizer_api_example.py
+++ pyspark_optimizer_api_example.py
@@ -1,6 +1,6 @@
-import requests
 import time
 import copy
+from auger_api import AugerAPI 
 
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
@@ -9,55 +9,6 @@ from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 spark = SparkSession.builder.appName("MLAugerSample").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
-
-#https://app-staging.auger.ai/api/v1/docs
-class AugerAPI:
-	def __init__(self, url):
-		self.url = url
-		self.token = None
-
-	def call_api(self, rest_method, api_command, data):
-		if self.token:
-			data['token'] = self.token
-
-		response = rest_method(self.url + "/api/v1/" + api_command, json=data).json()
-		return response.get('data', {})
-
-	def login(self, email, password):
-		response = self.call_api(requests.post, "tokens", {'email': email, 'password': password})
-		self.token = response.get('token')
-		print(self.token)
-
-	def create_trial_search(self, trials_total_count, search_space):
-		response = self.call_api(requests.post, "trial_searches", 
-			{'trials_total_count': trials_total_count, 'search_space': search_space,
-					'dataset_metafeatures':{'test': 1}})
-		self.search_id = response.get('id')
-
-		status = response.get('status')
-		print("Search ID: %s, status: %s"%(self.search_id, status))
-
-		while status != 'done' and status != 'error':
-			response = self.call_api(requests.get, "trial_searches/%s"%self.search_id, {})
-
-			status = response.get('status')
-			print("Search ID: %s, status: %s"%(self.search_id, status))
-
-		if status == 'error':
-			print("Error when create auger object:%s"%response)
-			exit(1)
-
-	def continue_trial_search(self, trials_limit, trials_history):
-		 return self.call_api(requests.patch, "trial_searches/%s"%self.search_id, 
-			{'trials_limit': trials_limit, 'trials_history': trials_history}).get('next_trials', [])
-
-	@staticmethod		 
-	def create_object_by_class(full_name, args):
-	    import importlib
-
-	    module_name, class_name = full_name.rsplit('.', 1)
-	    cls = getattr(importlib.import_module(module_name), class_name)
-	    return cls(**args)
 
 augerAPI = AugerAPI("https://app-staging.auger.ai")
 augerAPI.login(email="email", password="password")
@@ -77,23 +28,9 @@ search_space = {
     }
 }
 
-trials_history = [
-	{
-	  "score":0.4347826086956522,
-	  "evaluation_time":0.4013018608093262,
-	  "algorithm_name":"pyspark.ml.classification.RandomForestClassifier",
-	  "algorithm_params":{
-	    "maxDepth":15,
-	    "maxBins":40,
-	    "impurity":"gini",
-	    "numTrees":100
-	  }
-	}
-]
-
 trials_total_count = 20
 augerAPI.create_trial_search(trials_total_count=trials_total_count, search_space=search_space)
-next_trials = augerAPI.continue_trial_search(trials_limit=4, trials_history=trials_history)
+next_trials = augerAPI.continue_trial_search(trials_limit=4)
 
 data = spark.read.format("libsvm").load("sample_libsvm_data.txt")
 labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
@@ -101,6 +38,7 @@ featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures",
 
 (training_data, test_data) = data.randomSplit([0.8, 0.2])
 
+print("Start execute trials: %s"%next_trials)
 trials_history = []
 while len(trials_history) < trials_total_count:
 	#Execute trials to get score, bigger is better (0.0..1.0)
